13cce579ae6fa3be01cde7246cf22ae0d43c90a6
Aleem Rehmtulla <1rehmtullaal2@gmail.com>
2023-07-12 17:21:57 -0400

    clean up and opensource

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,42 @@
 import { Heading, VStack, SimpleGrid, Text } from "@chakra-ui/react";
-import Head from "next/head";
-
-type PlanetData = {
-  planet: string;
-  earthYearsPerYear: number;
-};
+import { NextSeo } from "next-seo";
 
 const PLANET_DATA = [
-  { planet: "Mercury", earthYearsPerYear: 0.24 },
+  { planet: "Mars", earthYearsPerYear: 1.88 },
   { planet: "Venus", earthYearsPerYear: 0.62 },
   { planet: "Earth", earthYearsPerYear: 1 },
-  { planet: "Mars", earthYearsPerYear: 1.88 },
-  { planet: "Jupiter", earthYearsPerYear: 11.86 },
-  { planet: "Saturn", earthYearsPerYear: 29.46 },
-  { planet: "Uranus", earthYearsPerYear: 83.7 },
-  { planet: "Neptune", earthYearsPerYear: 164 },
-  { planet: "Pluto", earthYearsPerYear: 248 },
   { planet: "Eris", earthYearsPerYear: 558 },
+  { planet: "Mercury", earthYearsPerYear: 0.24 },
+  { planet: "Pluto", earthYearsPerYear: 248 },
+  { planet: "Saturn", earthYearsPerYear: 29.46 },
+  { planet: "Neptune", earthYearsPerYear: 164 },
+  { planet: "Jupiter", earthYearsPerYear: 11.86 },
+  { planet: "Uranus", earthYearsPerYear: 83.7 },
   { planet: "Haumea", earthYearsPerYear: 283 },
   { planet: "Makemake", earthYearsPerYear: 309 },
-] as PlanetData[];
+];
 
 export default function Home() {
   const CURRENT_YEAR = new Date().getFullYear();
   return (
     <>
-      <Head>
-        <title>What Year Is It Right Now?</title>
-        <meta
-          name="description"
-          content="Things aleem builds: What years do they know? Do they know years? Let's find out!"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <NextSeo
+        title="What Year Is It Right Now?"
+        description="Things aleem builds: What years do they know? Do they know years? Let's find out!"
+      />
       <VStack
-        justifyContent="center"
         height={{ base: "90vh", md: "100vh" }}
+        justifyContent="center"
+        textAlign="center"
         spacing={8}
       >
-        <Heading
-          fontSize={{ base: 42, md: "6xl" }}
-          textAlign="center"
-          fontFamily="ness"
-          fontWeight="black"
-        >
+        <Heading fontSize={{ base: 42, md: "6xl" }} fontWeight="black">
           What Year Is It Right Now?
         </Heading>
 
-        <SimpleGrid
-          columns={{ base: 2, md: 3 }}
-          spacing={{ base: 2, md: 8 }}
-          px={{ base: 1, md: 0 }}
-          justifyContent="center"
-        >
+        <SimpleGrid columns={{ base: 2, md: 3 }} spacing={{ base: 2, md: 8 }}>
           {PLANET_DATA.map(({ planet, earthYearsPerYear }) => (
-            <Heading
-              fontFamily="ness"
-              key={planet}
-              fontWeight="normal"
-              size={{ base: "md", md: "lg" }}
-            >
+            <Text key={planet} fontSize={{ base: "lg", md: "3xl" }}>
               {planet}:{" "}
               {Math.floor((CURRENT_YEAR * earthYearsPerYear * 100) / 100) <
               10000
@@ -67,25 +44,16 @@ export default function Home() {
                 : Math.floor(
                     (CURRENT_YEAR * earthYearsPerYear * 100) / 100
                   ).toLocaleString()}
-            </Heading>
+            </Text>
           ))}
         </SimpleGrid>
 
         <VStack justifyContent="center">
-          <Text
-            fontSize={{ base: "lg", md: "xl" }}
-            textAlign="center"
-            fontWeight="bold"
-            fontFamily="ness"
-          >
-            Have you discovered a new planet?{" "}
+          <Text fontSize={{ base: "lg", md: "xl" }} fontWeight="bold">
+            Have you discovered a new planet?
           </Text>
-          <Text
-            fontSize={{ base: "md", md: "lg" }}
-            textAlign="center"
-            fontFamily="ness"
-          >
-            submitplanet@whatyearisitrightnow.com{" "}
+          <Text fontSize={{ base: "md", md: "lg" }}>
+            submitplanet@whatyearisitrightnow.com
           </Text>
         </VStack>
       </VStack>
---
+++ theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "ness",
+    body: "ness",
+  },
+});
+
+export default theme;
